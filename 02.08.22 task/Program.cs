7d2632c5fab8af4f940ab48e15947fe920e5c13b
evezmemmedli <avazkm@code.edu.az>
2022-02-08 22:14:43 +0400

    Add project files.

---
+++ 02.08.22 task/Program.cs
@@ -0,0 +1,50 @@
+﻿//bool a = false;
+//string word = "Teyinat";
+//foreach (var x in word)
+//{
+//    if (x == 'a')
+//    {
+//        a = true;
+//    }
+//}
+//Console.WriteLine(a);
+///////-------------------------------------------------------------
+
+//string sozz = "Qalebe";
+
+//for (int i = 0; i < sozz.Length; i++)
+//{
+//    if (sozz[i]=='a')
+//    {
+//        Console.WriteLine("a var");
+//        break;
+//    }
+//    else if (i == sozz.Length - 1)
+//    {
+//        Console.WriteLine("a yoxdur");
+//    }
+//}
+////////////////////--------------------------------------------
+///
+//string sozz = "Qalebe";
+//string herf = "a yoxdur";
+//for (int i = 0; i < sozz.Length; i++)
+//{
+//    if (sozz[i] == 'a')
+//    {
+//        herf = "a var";
+//    }
+//}
+//Console.WriteLine(herf);
+//////////////////--------------------------------------
+
+//int number = 5;
+//if (number%2==0)
+//{
+//    Console.WriteLine("cutdur");
+
+//}
+//else
+//{
+//    Console.WriteLine("tekdir");
+//}
